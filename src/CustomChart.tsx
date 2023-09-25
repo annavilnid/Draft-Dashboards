2925e2c903f6960a2a511b547bef7bc2890aa8ab
annavilnid <anna.vilnid@gmail.com>
2023-09-25 20:16:49 +0200

    test line

--- src/CustomChart.tsx
+++ src/CustomChart.tsx
@@ -1,34 +1,27 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine } from "recharts";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 
 const data = [
-    { name: 'Янв', uv: 4000, pv: 2400, amt: 2410 },
-    { name: 'Фев', uv: 2000, pv: 1398, amt: 2410 },
-    { name: 'Март', uv: 4000, pv: 1398, amt: 2410 },
-    { name: 'Апрель', uv: 3000, pv: 1398, amt: 2410 },
-    // Другие данные...
+    { time: '00:00', value: 0 },
+    { time: '00:00', value: 1000 },
+    { time: '00:00', value: 2000 },
+    { time: '00:00', value: 1500 },
 ];
 
 function CustomChart() {
     return (
-        <LineChart width={400} height={300} data={data}>
-            <XAxis dataKey="name" />
+        <LineChart width={800} height={400} data={data}>
+            <XAxis dataKey="time" interval={0} />
             <YAxis />
             <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="amt" stroke="#8884d8" />
-            <ReferenceLine y={1500} label="Test" stroke="red" strokeDasharray="3 3" />
+            <Line type="monotone" dataKey="value" stroke="#8884d8" name="Значения" dot={false} />
             <Tooltip />
             <Legend />
         </LineChart>
-        // <LineChart width={500} height={300} data={data}>
-        //     <XAxis dataKey="name"/>
-        //     <YAxis/>
-        //     <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-        //     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        //     <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-        // </LineChart>
     );
 }
 
 export default CustomChart;
+
+
+
+
