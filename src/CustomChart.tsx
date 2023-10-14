737381eba098549ba37911f7dffc3453c8a403e2
annavilnid <anna.vilnid@gmail.com>
2023-10-14 22:01:20 +0200

    add test

--- src/CustomChart.tsx
+++ src/CustomChart.tsx
@@ -19,6 +19,7 @@ const data = [
     { time: '00:00', value: 1500 },
     { time: '00:00', value: 1500 },
     { time: '00:00', value: 1500 },
+    { time: '00:00', value: 1500 },
 ];
 
 function CustomChart() {
